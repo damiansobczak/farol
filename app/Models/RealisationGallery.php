e155706a113867a47b509d126534449b4c372907
Mariusz Baranowski <Mariusz.Baranowski.it@gmail.com>
2020-11-22 19:19:50 +0100

    filled structure of realisation gallery

--- app/Models/RealisationGallery.php
+++ app/Models/RealisationGallery.php
@@ -7,5 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class RealisationGallery extends Model
 {
-    use HasFactory;
+	use HasFactory;
+	protected $fillable = [
+		'image',
+		'imageAlt',
+		'video',
+		'realisationId',
+	];
 }
--- database/migrations/2020_11_22_165212_create_realisation_galleries_table.php
+++ database/migrations/2020_11_22_165212_create_realisation_galleries_table.php
@@ -6,26 +6,30 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateRealisationGalleriesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('realisation_galleries', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('realisation_galleries', function (Blueprint $table) {
+			$table->id();
+			$table->string('image');
+			$table->string('imageAlt');
+			$table->string('video');
+			$table->integer('realisationId');
+			$table->timestamps();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('realisation_galleries');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('realisation_galleries');
+	}
 }
