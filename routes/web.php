8f9e1ace20fa4e065c76e68402cb2b8abe1349a4
Damian Sobczak <damiansobczak@MacBook-Pro-Damian.local>
2020-11-22 14:19:47 +0100

    Login and forgot views for admin

--- routes/web.php
+++ routes/web.php
@@ -26,5 +26,7 @@ Route::view('/', 'welcome');
 */
 
 Route::prefix('admin')->group(function() {
-    Route::get('/settings', [SettingsController::class, 'index']);
+    Route::view('/', 'admin.auth.login');
+    Route::view('/przypomnienie', 'admin.auth.forgot');
+    Route::get('/ustawienia', [SettingsController::class, 'index']);
 });
