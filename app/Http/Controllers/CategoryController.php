9b7a881e7e65c3867f23a2ad3ce8fb98d564cf0e
Mariusz Baranowski <Mariusz.Baranowski.it@gmail.com>
2021-01-06 18:53:32 +0100

    Unify category view and contoller; Routing modified

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,45 +4,43 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Category;
+use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends Controller
 {
+	public function validator($data)
+	{
+		return Validator::make($data, [
+			"name" => "required|string|unique:categories,name",
+			"image" => "nullable|image",
+			"imageAlt" => "nullable|string"
+		]);
+	}
 	public function index()
 	{
-		$categories = Category::select('id', 'name')->get();
-		return view('admin.pages.category.index', ['categories' => $categories]);
+		$categories = Category::select('id', 'image', 'name')->get();
+		return view('admin.pages.category.index', compact('categories'));
 	}
-	public function form($categoryId = NULL)
+	public function create()
 	{
-		$categoryData = [];
-		if(isset($categoryId))
-			$categoryData = Category::find($categoryId)->first();
-		return view('admin.pages.category.form', ['categoryData' => $categoryData, 'categoryId' => $categoryId]);
+		return view('admin.pages.category.form');
 	}
-	public function store(Request $req, $categoryId = NULL)
+	public function store(Request $req)
 	{
-		if(isset($categoryId))
-		{
-			$req->validate([
-				"name" => "required|string",
-				"image" => "nullable|image",
-				"imageAlt" => "nullable|string"
-			]);
-			$category = Category::find($categoryId);
-		}
-		else
-		{
-			$req->validate([
-				"name" => "required|string|unique:categories,name",
-				"image" => "nullable|image",
-				"imageAlt" => "nullable|string"
-			]);
-			$category = new Category();
-		}
-		$category->name = $req->name;
-		$category->image = $req->image;
-		$category->imageAlt = $req->imageAlt;
-		$category->save();
+		$validated = $this->validator($req->all())->validate();
+		$category = Category::create($validated);
+		return redirect()->route('admin.categories');
+	}
+	public function edit($categoryId)
+	{
+		$category = Category::findOrFail($categoryId);
+		return view('admin.pages.category.form', compact('category'));
+	}
+	public function update(Request $req, $categoryId)
+	{
+		$category = Category::findOrFail($categoryId);
+		$validated = $this->validator($req->all())->validate();
+		$category->update($validated);
 		return redirect()->route('admin.categories');
 	}
 }
--- resources/views/admin/pages/category/form.blade.php
+++ resources/views/admin/pages/category/form.blade.php
@@ -2,13 +2,36 @@
 @section('title', 'Kategorie')
 @section('content')
 	<x-breadcrumbs :crumbs="[['name' => 'Kategorie', 'url' => route('admin.categories')], ['name' => 'Formularz kategorii', 'url' => '']]" />
-	<div class="rounded shadow-sm bg-white p-8">
-		<form action="@if(isset($categoryId)) {{ route('admin.categories.form.editSave', $categoryId) }} @else {{ route('admin.categories.form.save') }} @endif" method="POST">
-			@csrf
-			<input type="text" name="name" value="@if($errors->any()){{ old('name') }}@else{{ $categoryData->name ?? NULL }}@endif">
-			<input type="file" name="image" value="@if($errors->any()){{ old('image') }}@else{{ $categoryData->image ?? NULL }}@endif">
-			<input type="text" name="imageAlt" value="@if($errors->any()){{ old('imageAlt') }}@else{{ $categoryData->imageAlt ?? NULL }}@endif">
-			<button type="submit">Zapisz</button>
-		</form>
-	</div>
+	<form action="@if(isset($category->id)) {{ route('admin.categories.editSave', $category->id) }} @else {{ route('admin.categories.save') }} @endif" method="POST">
+		@csrf
+		@if(isset($category->id))
+			{{ method_field('PUT') }} 
+		@endif
+		<div class="flex">
+			<button type="submit" class="rounded px-4 py-2 bg-indigo-500 text-white mb-4">Zapisz</button>
+		</div>
+		<div class="flex rounded shadow-sm bg-white p-8">
+			<div class="flex flex-col flex-1 border-gray-200 pr-6">
+				@if(isset($category) && $category->image)
+					<img src="{{ $category->image }}" class="object-cover bg-gray-100 h-64 rounded">
+				@else
+					<div class="object-cover bg-gray-100 h-64 rounded"></div>
+				@endif
+				<label for="image" class="text-sm text-gray-400 mt-4 block">
+					Obrazek
+					<input type="file" name="image" id="image" value="@if($errors->any()){{ old('image') }}@else{{ $category->image ?? NULL }}@endif" class="font-light hover:border-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 placeholder-gray-300 w-full p-3 rounded border border-gray-200">
+				</label>
+				<label for="imageAlt" class="text-sm text-gray-400 mt-4 block">
+					Opis obrazka
+					<input type="text" name="imageAlt" id="imageAlt" value="@if($errors->any()){{ old('imageAlt') }}@else{{ $category->imageAlt ?? NULL }}@endif" class="font-light hover:border-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 placeholder-gray-300 w-full p-3 rounded border border-gray-200">
+				</label>
+			</div>
+			<div class="flex-1 flex-col pl-6">
+				<label for="name" class="text-sm text-gray-400 mt-4 block">
+					Nazwa
+					<input type="text" name="name" value="@if($errors->any()){{ old('name') }}@else{{ $category->name ?? NULL }}@endif" class="font-light hover:border-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 placeholder-gray-300 w-full p-3 rounded border border-gray-200">
+				</label>
+			</div>
+		</div>
+	</form>
 @endsection
