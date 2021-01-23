b9421ca3857762d533b707dd0329cb482e278862
Damian Sobczak <damiansobczak@MacBook-Pro-Damian.local>
2021-01-23 13:35:53 +0100

    Company subpage styling

---
+++ resources/views/components/locations.blade.php
@@ -0,0 +1,14 @@
+<div class="max-w-fluid mx-auto bg-gray-100 py-6 rounded">
+    <div class="container mx-auto flex items-center">
+        <a href="{{ route('main') }}" class="flex items-center text-gray-800 font-semibold text-sm hover:underline">
+            Strona Główna
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+                class="mx-2 w-4 h-4">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+            </svg>
+        </a>
+        <div href="#" class="flex items-center text-gray-500 text-sm">
+            O firmie
+        </div>
+    </div>
+</div>
---
+++ resources/views/pages/company.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('title', 'Dowiedz się więcej o naszej firmie')
+
+@section('content')
+@include('components.header')
+@include('components.locations')
+@include('components.about')
+@include('components.footer')
+@endsection
