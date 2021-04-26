7e33e885fde33e288ee2ad0d99acbde3a8ddc81f
Damian Sobczak <damiansobczak@MacBook-Pro-Damian.local>
2021-04-26 17:57:54 +0200

    Front filtering

--- app/Http/Livewire/Controls.php
+++ app/Http/Livewire/Controls.php
@@ -3,39 +3,65 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Str;
 
 class Controls extends Component
 {
 	//Data
 	public $product;
+
+	//Computed
 	public $collections;
 	public $materials;
-	public $materialsCopy;
 	public $colors;
+
 	//Material controls
-	public $searchMaterial;
-	public $totalMaterials = 0;
+	public $search;
+	public $selectedColor;
+	public $selectedCollection;
 
-	public function mount($product)
+	public function searchMaterial()
 	{
-		$this->product = $product;
-		$this->collections = $this->product->collections;
-		$this->materials = $this->collections->pluck('materials')->collapse();
-		$this->materialsCopy = $this->materials;
-		$this->colors = $this->materials->pluck('color')->collapse();
-		$this->totalMaterials = $this->materials->count();
-	}
-
-	public function search()
-	{
-		$value = $this->searchMaterial;
-		$this->materials = $this->materialsCopy->filter(function($material) use ($value) {
-			return false !== stripos($material['name'], $value);
-		});
+		//
 	}
 
 	public function render()
 	{
-		return view('livewire.controls');
+		$this->collections = $this->product->collections;
+		$this->materials = $this->product->collections->pluck('materials')->collapse();
+		$this->colors = $this->materials->map(function ($item) {
+			return $item->color;
+		});
+
+		$this->filtered = $this->materials;
+
+		$this->filtered = $this->filtered
+			->when($this->selectedColor, function ($query) {
+				return $query->filter(function ($value, $key) {
+					return $value->color->id == $this->selectedColor;
+				});
+			});
+
+		$this->filtered = $this->filtered
+			->when($this->selectedCollection, function ($query) {
+				return $query->filter(function ($value, $key) {
+					return $value->collection->id == $this->selectedCollection;
+				});
+			});
+
+		$this->filtered = $this->filtered
+			->when($this->search, function ($query) {
+				return $query->filter(function ($value, $key) {
+					return Str::contains($value->name, $this->search);
+				});
+			});
+
+
+		return view('livewire.controls', [
+			"collections" => $this->collections,
+			"materials" => $this->materials,
+			"colors" => $this->colors,
+			"filtered" => $this->filtered
+		]);
 	}
 }
--- resources/views/admin/pages/groups/index.blade.php
+++ resources/views/admin/pages/groups/index.blade.php
@@ -3,10 +3,9 @@
 @section('content')
 <x-breadcrumbs
     :crumbs="[['name' => 'Grupy', 'url' => route('admin.groups')], ['name' => 'Formularz grup', 'url' => '']]" />
-<div class="w-full flex space-x-4">
+<div class="w-full flex space-x-4 items-start">
     <div class="rounded shadow-sm bg-white p-8 flex w-1/2 flex-wrap">
-        <div
-            class="flex items-center w-full justify-between font-medium text-sm text-gray-600 border-b border-gray-200 pb-4 mb-4">
+        <div class="w-full font-medium text-sm text-gray-600 border-b border-gray-200 pb-4 mb-4">
             Kolory
         </div>
         @livewire('colors', ['colors' => $colors])
